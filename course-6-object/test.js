fbff99cc2fccf387afe356919c24c0493df3f9cc
yul <1103221645@qq.com>
2022-10-14 23:40:40 +0800

    继承

--- course-6-object/test.js
+++ course-6-object/test.js
@@ -2,16 +2,11 @@ function Person(name) {
   this.name = name
 }
 
-function _new() {
-  const obj = new Object()
-
-  let FunctionConstructor = [].shift.call(arguments) // 取出第一个参数，即构造函数
-  obj.__proto__ = FunctionConstructor.prototype
-
-  let resultObj = FunctionConstructor.apply(obj, arguments)
-
-  return typeof resultObj === 'object' ? resultObj : obj
+Person.prototype.say = function () {
+  console.log(this.name)
 }
 
-const p1 = _new(Person, 'xxx')
-console.log(p1)
+function Kobe() {}
+
+// 继承
+Kobe.prototype = new Person()
