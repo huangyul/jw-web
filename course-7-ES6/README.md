13284f20ec2469bca397b9b10d43a602f9b86e46
yul <1103221645@qq.com>
2022-11-23 06:26:39 +0800

    es6 promise allsettled

--- course-7-ES6/README.md
+++ course-7-ES6/README.md
@@ -309,4 +309,40 @@ function getObjectEntries(obj) {
 
 #### Promise
 
- 
+Promise.allSettled 返回所有 `promise` 的状态和结果
+
+```js
+function PromsieAllSettled(promiseArray) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(promiseArray)) {
+      return reject(new TypeError('参数必须是一个数组'))
+    }
+
+    let counter = 0
+    const len = promiseArray.length
+    const res = []
+
+    for (let i = 0; i < len; i++) {
+      Promise.resolve(promiseArray[i])
+        .then((value) => {
+          res[i] = {
+            value,
+            status: 'fulfilled',
+          }
+        })
+        .catch((reason) => {
+          res[i] = {
+            reason,
+            status: 'rejected',
+          }
+        })
+        .finally(() => {
+          counter++
+          if (counter == len) {
+            resolve(res)
+          }
+        })
+    }
+  })
+}
+```
--- course-7-ES6/index.js
+++ course-7-ES6/index.js
@@ -1,11 +1,33 @@
-function shallowClone(source) {
-  const obj = {}
-  for (let key in source) {
-    if (source.hasOwnProperty(key)) {
-      obj[key] = source[key]
+function PromsieAllSettled(promiseArray) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(promiseArray)) {
+      return reject(new TypeError('参数必须是一个数组'))
     }
-  }
-  return obj
-}
 
-console.log(shallowClone({ name: 1, age: 3 }))
+    let counter = 0
+    const len = promiseArray.length
+    const res = []
+
+    for (let i = 0; i < len; i++) {
+      Promise.resolve(promiseArray[i])
+        .then((value) => {
+          res[i] = {
+            value,
+            status: 'fulfilled',
+          }
+        })
+        .catch((reason) => {
+          res[i] = {
+            reason,
+            status: 'rejected',
+          }
+        })
+        .finally(() => {
+          counter++
+          if (counter == len) {
+            resolve(res)
+          }
+        })
+    }
+  })
+}
