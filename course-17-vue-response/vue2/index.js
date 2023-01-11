2541a081b98648f021dc83b130a38e73fdd50330
yul <1103221645@qq.com>
2023-01-12 06:16:47 +0800

    vue2 拦截对象

--- course-17-vue-response/vue2/index.js
+++ course-17-vue-response/vue2/index.js
@@ -8,7 +8,11 @@ export class Vue {
     this.$data = options.data
     this.$methods = options.methods
 
+    // 代理一下data
     this.proxy(this.$data)
+
+    // 拦截this.$data
+    new Observer(this.$data)
   }
 
   // 代理一下，可以直接使用data里面的值   this.$data.xx -> this.xxx
@@ -23,11 +27,45 @@ export class Vue {
         set(newValue) {
           // 如果等于本身，就不需要重新设置值了
           // 还要注意NaN不等于本身
-          if (newValue === data[key] || (isNaN(newValue) && isNaN(data[key])))
-            return
+          if (newValue === data[key] || __isNaN(newValue, data[key])) return
           data[key] = newValue
         },
       })
+    })
+  }
+}
+
+function __isNaN(a, b) {
+  return Number.isNaN(a) && Number.isNaN(b)
+}
+class Observer {
+  constructor(data) {
+    this.walk(data)
+  }
+  walk(data) {
+    if (!data || typeof data !== 'object') return
+    Object.keys(data).forEach((key) => {
+      this.defineReactive(data, key, data[key])
+    })
+  }
+  defineReactive(obj, key, value) {
+    let that = this
+    this.walk(value) // 因为值也可能是对象，也需要变成响应式
+
+    Object.defineProperty(obj, key, {
+      configurable: true,
+      enumerable: true,
+      get() {
+        return value
+      },
+      set(newValue) {
+        // 判断NaN
+        if (__isNaN(value, newValue)) return
+        if (newValue === value) return
+        value = newValue
+        // 新的值也可能是对象
+        this.walk(newValue)
+      },
     })
   }
 }
