cc081dc3015bf4f31aedc560f816b7cd85c3739f
yul <1103221645@qq.com>
2023-02-07 06:33:40 +0800

    define

--- course-2-js-module/AMD/require.js
+++ course-2-js-module/AMD/require.js
@@ -4,5 +4,13 @@ const defaultOptions = {
   paths: '',
 }
 
+// 为啥不写let const var？为了能挂载到全局
+// 正确的写法 window.rj = {}
+rj = {}
+
 // config的时候获取到options的配置
 rj.config = (options) => Object.assign(defaultOptions, options)
+
+define = (name, deps, factory) => {
+  // todo参数的判断，互换
+}
